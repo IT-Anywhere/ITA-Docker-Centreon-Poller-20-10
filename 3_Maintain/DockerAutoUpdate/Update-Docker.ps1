d1459030430a2f63f9dc8aba1d98ec15938bb527
ITASergio <74178230+ITASergio@users.noreply.github.com>
2020-12-11 11:02:12 +0100

    Update Update-Docker.ps1

---
+++ 3_Maintain/DockerAutoUpdate/Update-Docker.ps1
@@ -0,0 +1,109 @@
+#Script to keep docker automatically updated
+#Written by Sergio Padure for IT-Anywhere
+#Based on https://github.com/damienvanrobaeys/Docker_Desktop_CE_Auto_Update_Service
+#Requires -RunAsAdministrator
+#Requires -Version 5.1
+#Requires -PSEdition Desktop
+
+#Preparing logging and general variables
+$scriptdir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+$WorkingDirectory = "C:\temp\docker"
+$WDExists = Test-Path -Path $WorkingDirectory
+if (-not $WDExists) {
+    New-Item -ItemType Directory -Path $WorkingDirectory
+}
+$dateandtime = Get-Date -Format "dd_MM_yyyy_HH-mm"
+
+$Log_File_Full = "$WorkingDirectory\Debug\Update_Docker_Full_Log.log"
+$Log_File = "$WorkingDirectory\Debug\Docker_Update.log"
+$Appli_name = "Docker for Windows"
+
+If (!(test-path $Log_File)) { new-item $Log_File -type file -force }
+$Global:Current_Folder = split-path $MyInvocation.MyCommand.Path
+
+#Preparing functions
+Function Write_Log {
+    param(
+        $Message_Type,	
+        $Message
+    )
+		
+    $MyDate = "[{0:MM/dd/yy} {0:HH:mm:ss}]" -f (Get-Date)		
+    Add-Content $Log_File  "$MyDate - $Message_Type : $Message"			
+}
+Function Get_Info_Beetween_Strings {
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory = $false)]	
+        [string]$version
+    )	
+    $Check_Pattern = ">(.*?)</a>"
+    $Get_Info = ([regex]::match($version, $Check_Pattern).Groups[1].Value).Trim()
+    Return $Get_Info
+}
+
+Write_Log -Message_Type "INFO" -Message "Starting $Appli_name update process"											
+
+$New_Version_available = $False
+$Docker_Reg_Path = "HKLM:\software\microsoft\windows\currentversion\uninstall\Docker desktop"
+
+If (Test-Path $Docker_Reg_Path) {
+    $Get_Current_Docker_Version = (Get-ItemProperty -Path "HKLM:\software\microsoft\windows\currentversion\uninstall\Docker desktop").displayversion
+    $Current_Docker_CE_Version = "docker desktop community $Get_Current_Docker_Version"
+    $Release_Notes_Page = "https://docs.docker.com/docker-for-windows/release-notes/"
+    $Get_Docker_CE_Versions = ((Invoke-WebRequest -Uri $Release_Notes_Page -UseBasicParsing).links | Where { $_.outerhtml -like "*docker desktop community*" }).outerhtml
+    $Get_Latest_CE_Version = $Get_Docker_CE_Versions[0]
+    $Docker_available_Version = Get_Info_Beetween_Strings -version $Get_Latest_CE_Version
+    If ($Docker_available_Version -gt $Current_Docker_CE_Version) {
+        Write_Log -Message_Type "INFO" -Message "A new version of $Appli_name is available"	
+        Write_Log -Message_Type "INFO" -Message "This version is: $Docker_available_Version"		
+        $New_Version_available = $True
+    }				
+
+    If ($New_Version_available -eq $True) {
+        Try {
+            $Docker_New_Version = $Docker_available_Version.Split(" ")[-1]
+            Write_Log -Message_Type "INFO" -Message "Starting Update"
+            $Run_Update_Status = $True
+        }
+        Catch {
+            $Run_Update_Status = $False
+        }
+
+        If ($Run_Update_Status -eq $True) {
+            $New_Version_Download_Status = $False
+            $New_Docker_Version_Path = "$WorkingDirectory\Docker_Desktop_Installer.exe"
+            $Docker_Installer_Link = "https://download.docker.com/win/stable/Docker%20Desktop%20Installer.exe"
+            Write_Log -Message_Type "INFO" -Message "The new version will be downloaded"
+            Try {							
+                $Download_EXE = new-object -typename system.net.webclient
+                $Download_EXE.Downloadfile($Docker_Installer_Link, $New_Docker_Version_Path)									
+                $New_Version_Download_Status = $True
+                Write_Log -Message_Type "SUCCESS" -Message "The new version of $Appli_name has been successfully downloaded"																								
+            }
+            Catch {							
+                $New_Version_Download_Status = $False
+                Write_Log -Message_Type "ERROR" -Message "An issue occured while downloading the new version of $Appli_name"																																		
+            }							
+        }
+
+    }
+    Else {
+        Write_Log -Message_Type "INFO" -Message "There is no new version for $Appli_name"						
+    }
+
+    If (($New_Version_Download_Status -eq $True) -and ($Run_Update_Status -eq $True)) {
+        Write_Log -Message_Type "INFO" -Message "The new version will be installed"	
+        
+        Try {			
+            start-process -FilePath $New_Docker_Version_Path -ArgumentList "install --quiet" -RedirectStandardOutput $Log_File_Full -Wait
+        }
+        Catch {				
+            Write_Log -Message_Type "ERROR" -Message "An issue occured while installing the new version of $Appli_name"																
+        }		
+    }	
+}
+Else {
+    Write_Log -Message_Type "ERROR" -Message "$Appli_name is not installed on your computer"																
+}
